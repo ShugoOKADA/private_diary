e11815660f9bb9ef6182dcd4ccfb9ccc332685b3
okada <shugo140208@softbank.ne.jp>
2021-09-16 13:34:08 +0900

    日記一覧表示機能

--- config/settings.py
+++ config/settings.py
@@ -156,3 +156,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
 
+LOGIN_REDIRECT_URL ='diary:diary_list'
--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import static
 from django.urls import path,include
 
-from . import settings_common, settings_dev
+from . import settings, settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +10,4 @@ urlpatterns = [
     path('accounts/',include('allauth.urls')),
 ]
 
-urlpatterns += static(settings_common.MEDIA_URL,document_root=settings_dev.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
